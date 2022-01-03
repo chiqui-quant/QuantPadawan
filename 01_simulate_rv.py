5ad33b58947133f272f4c8e9268ea15be9081d2f
Chiqui <christiansposi@gmail.com>
2022-01-03 16:57:36 +0100

    initial commit

---
+++ 01_simulate_rv.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 20 21:12:50 2021
+
+@author: Chiqui
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib as mpl 
+import scipy
+import importlib
+import matplotlib.pyplot as plt
+from scipy.stats import skew, kurtosis, chi2
+
+nb_sims = 10**6
+df = 2
+dist_name = 'chi-square' # student normal exponential uniform chi-square
+
+if dist_name == 'normal':
+    x = np.random.standard_normal(nb_sims)
+    x_str = dist_name
+elif dist_name == 'exponential':
+    x = np.random.standard_exponential(nb_sims)
+    x_str = dist_name
+elif dist_name == 'uniform':
+    x = np.random.uniform(0,1,nb_sims)
+    x_str = dist_name
+elif dist_name == 'student':
+    x = np.random.standard_t(df=df,size=nb_sims)
+    x_str = dist_name + ' | df = ' + str(df)
+elif dist_name == 'chi-square':
+    x = np.random.chisquare(df=df,size=nb_sims)
+    x_str = dist_name + ' | df = ' + str(df)
+
+
+# plot histogram
+plt.figure()
+plt.hist(x,bins=100) # bins is the number of segments
+plt.title(x_str)
+plt.show()
