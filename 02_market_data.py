a1550642cce3f35be4804b192f5bbbd04c732250
Chiqui <christiansposi@gmail.com>
2022-02-19 23:11:08 +0100

    working normality test using market data

--- 02_market_data.py
+++ 02_market_data.py
@@ -14,33 +14,26 @@ ric = 'BBVA.MC'
 path = directory + ric + '.csv'
 raw_data = pd.read_csv(path)
 
+# Use prices
+# x = raw_data['Close'].values
+# x_description = 'market data ' + ric
+# nb_rows = len(x) # alternatively nb_rows = raw_data.shape[0]
 
+# Note: but we want to use returns for the normality test
+# Create table of returns
+t = pd.DataFrame() # t (for table) with no elements
+t['date'] = pd.to_datetime(raw_data['Date'], dayfirst=True) # if dayfirst is omitted we have American dates
+t['close'] = raw_data['Close']
+t.sort_values(by='date',ascending=True)
+t['close_previous'] = t['close'].shift(1) # shift moves values below by the amount you indicate
+t['return_close'] = t['close']/t['close_previous'] - 1
+t = t.dropna()
+t = t.reset_index(drop=True)
 
+x = t['return_close'].values
+x_description = 'market data ' + ric
+nb_rows = len(x)
 
-
-
-
-
-nb_sims = 10**6
-df = 5
-dist_name = 'normal' # student normal exponential uniform chi-square
-dist_type = 'simulated RV' # real custom
-
-if dist_name == 'normal':
-    x = np.random.standard_normal(nb_sims)
-    x_description = dist_type + ' ' + dist_name
-elif dist_name == 'exponential':
-    x = np.random.standard_exponential(nb_sims)
-    x_description = dist_type + ' ' + dist_name
-elif dist_name == 'uniform':
-    x = np.random.uniform(0,1,nb_sims)
-    x_description = dist_type + ' ' + dist_name
-elif dist_name == 'student':
-    x = np.random.standard_t(df=df,size=nb_sims)
-    x_description = dist_type + ' ' + dist_name + ' | df = ' + str(df)
-elif dist_name == 'chi-square':
-    x = np.random.chisquare(df=df,size=nb_sims)
-    x_description = dist_type + ' ' + dist_name + ' | df = ' + str(df)
 
 '''
 Goal: create a Jarque-Bera normality test
@@ -50,7 +43,7 @@ x_mean = np.mean(x)
 x_std = np.std(x)
 x_skew = skew(x)
 x_kurtosis = kurtosis(x) # excess kurtosis
-x_jb_stat = nb_sims/6*(x_skew**2 + 1/4*x_kurtosis**2)
+x_jb_stat = nb_rows/6*(x_skew**2 + 1/4*x_kurtosis**2)
 x_p_value = 1 - chi2.cdf(x_jb_stat, df=2) 
 x_is_normal = (x_p_value > 0.05) # equivalently jb < 6
 
