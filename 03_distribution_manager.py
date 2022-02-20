1c9220c7115b09cc932949901694528d0ea3358a
Chiqui <christiansposi@gmail.com>
2022-02-20 10:25:17 +0100

    working functions load_timeseries and plot_histogram in distribution_manager class

--- 03_distribution_manager.py
+++ 03_distribution_manager.py
@@ -11,12 +11,15 @@ import file_classes
 importlib.reload(file_classes)
 
 inputs = {
-    "data_type" : "simulation", # simulation real custom
-    "variable_name" : "student", # normal student chi-square SPY
-    "degrees_freedom" : 9, # only in student and chi-square
-    "ns_sims" : 10**6 # for simulated random variables
+    "data_type" : "real", # simulation real custom
+    "variable_name" : "BBVA.MC", # normal student chi-square exponential BBVA.MC
+    "degrees_freedom" : None, # only in student and chi-square
+    "nb_sims" : None, # for simulated random variables
 } # this is a dictionary with the input variables
 
 dm = file_classes.distribution_manager(inputs)
 dm.load_timeseries() # polymorphism
-dm.compute()
+dm.plot_histogram()
+
+# dm.compute()
+# print(dm)
--- file_classes.py
+++ file_classes.py
@@ -7,5 +7,71 @@ import matplotlib.pyplot as plt
 from scipy.stats import skew, kurtosis, chi2
 
 class distribution_manager():
+    
     def __init__(self, inputs):
         self.inputs = inputs
+        self.data_table = None
+        self.description = None
+        self.nb_rows = None
+
+    def load_timeseries(self):
+        
+        data_type = self.inputs['data_type']
+        
+        if data_type == 'simulation':
+            nb_sims = self.inputs['nb_sims']
+            dist_name = self.inputs['variable_name']
+            degrees_freedom = self.inputs['degrees_freedom']
+            if dist_name == 'normal':
+                x = np.random.standard_normal(nb_sims)
+                x_description = data_type + ' ' + dist_name
+            elif dist_name == 'exponential':
+                x = np.random.standard_exponential(nb_sims)
+                x_description = data_type + ' ' + dist_name
+            elif dist_name == 'uniform':
+                x = np.random.uniform(0,1,nb_sims)
+                x_description = data_type + ' ' + dist_name
+            elif dist_name == 'student':
+                x = np.random.standard_t(df=degrees_freedom,size=nb_sims)
+                x_description = data_type + ' ' + dist_name + ' | df = ' + str(degrees_freedom)
+            elif dist_name == 'chi-square':
+                x = np.random.chisquare(df=degrees_freedom,size=nb_sims)
+                x_description = data_type + ' ' + dist_name + ' | df = ' + str(degrees_freedom)
+                
+            self.description = x_description
+            self.nb_rows = nb_sims
+            self.vec_returns = x
+
+
+        elif data_type == 'real':
+            directory = 'C:\\Users\\Chiqui\\Desktop\\Python Projects\\QuantPadawan\\data\\'
+
+            ric = self.inputs['variable_name']
+            path = directory + ric + '.csv'
+            raw_data = pd.read_csv(path)
+
+            t = pd.DataFrame() # t (for table) with no elements
+            t['date'] = pd.to_datetime(raw_data['Date'], dayfirst=True) # if dayfirst is omitted we have American dates
+            t['close'] = raw_data['Close']
+            t.sort_values(by='date',ascending=True)
+            t['close_previous'] = t['close'].shift(1) # shift moves values below by the amount you indicate
+            t['return_close'] = t['close']/t['close_previous'] - 1
+            t = t.dropna()
+            t = t.reset_index(drop=True)
+            
+            self.data_table = t
+            self.description = 'market data ' + ric
+            self.nb_rows = t.shape[0]
+            self.vec_returns = t['return_close'].values
+
+    def plot_histogram(self):
+        plt.figure()
+        plt.hist(self.vec_returns,bins=100) # bins is the number of segments
+        plt.title(self.description)
+        plt.show()
+
+
+
+
+
+
