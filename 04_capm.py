46b2c623f369fdd2397092c9c64f7b7de5941910
Chiqui <christiansposi@gmail.com>
2022-02-21 18:22:10 +0100

    created function for loading timeseries

---
+++ 04_capm.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib as mpl 
+import scipy
+import importlib
+import matplotlib.pyplot as plt
+from scipy.stats import skew, kurtosis, chi2
+
+# Import our own files and reload
+import file_classes
+importlib.reload(file_classes)
