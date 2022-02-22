c96233693dea456b64c9a4bee3058398aee1b0cf
Chiqui <christiansposi@gmail.com>
2022-02-22 22:03:38 +0100

    created minimize test

---
+++ 06_minimize_test.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib as mpl 
+import scipy
+import importlib
+import matplotlib.pyplot as plt
+from scipy.stats import skew, kurtosis, chi2, linregress
+from scipy.optimize import minimize
