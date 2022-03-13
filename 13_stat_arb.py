feac6058a1689ec368307dde1f6038a11ef05e55
Chiqui <christiansposi@gmail.com>
2022-03-13 22:27:03 +0100

    implemented mean reversion strategy

--- 13_stat_arb.py
+++ 13_stat_arb.py
@@ -86,3 +86,107 @@ plt.plot(timestamps, spread, color='black', label='spread')
 plt.legend(loc=0)
 plt.grid()
 plt.show()
+
+# Loop for backtest
+size = timeseries.shape[0]
+columns = ['position','entry_signal','exit_signal', 'pnl_daily', 'trade', 'pnl_trade']
+position = 0
+entry_spread = 0.
+can_trade = False
+mtx_backtest = np.zeros((size,len(columns)))
+for n in range(size):
+    # input data for the day
+    spread = timeseries['spread'][n]
+    spread_previous = timeseries['spread_previous'][n]
+    lower_2 = timeseries['lower_2'][n]
+    lower_1 = timeseries['lower_1'][n]
+    mean = timeseries['mean'][n]
+    upper_1 = timeseries['upper_1'][n]
+    upper_2 = timeseries['upper_2'][n]
+    # reset output data for the day
+    pnl_daily = 0.
+    trade = 0
+    pnl_trade = 0.
+    # check if we can trade
+    if not can_trade:
+        can_trade = position == 0 and spread > lower_1 and spread < upper_1
+    if not can_trade:
+        continue
+    # enter new position
+    if position == 0:
+        entry_signal = 0
+        exit_signal = 0
+        if spread > lower_2 and spread < lower_1:
+            entry_signal = 1 # buy signal
+            position = 1 
+            entry_spread = spread
+        elif spread > upper_1 and spread < upper_2:
+            entry_signal = -1 # sell signal
+            position = -1
+            entry_spread = spread
+    # exit long position
+    elif position == 1:
+        entry_signal = 0
+        pnl_daily = position*(spread - spread_previous)
+        if n == size-1 or spread > mean or spread < lower_2:
+            exit_signal = 1 # last day, take profit or stop loss
+            pnl_trade = position*(spread-entry_spread)
+            position = 0
+            trade = 1
+            can_trade = False
+        else:
+            exit_signal = 0
+    # exit short position
+    elif position == -1:
+        entry_signal = 0
+        pnl_daily = position*(spread - spread_previous)
+        if n == size-1 or spread < mean or spread > upper_2:
+            exit_signal = 1 # last day, take profit or stop loss
+            pnl_trade = position*(spread-entry_spread)
+            position = 0
+            trade = 1
+            can_trade = False
+        else:
+            exit_signal = 0    
+
+    # save data for the day
+    m = 0
+    mtx_backtest[n][m] = position
+    m = m + 1 
+    mtx_backtest[n][m] = entry_signal
+    m = m + 1 
+    mtx_backtest[n][m] = exit_signal
+    m = m + 1 
+    mtx_backtest[n][m] = pnl_daily
+    m = m + 1 
+    mtx_backtest[n][m] = trade
+    m = m + 1 
+    mtx_backtest[n][m] = pnl_trade
+    
+df2 = pd.DataFrame(data=mtx_backtest,columns=columns)
+df = pd.concat([timeseries,df2],axis = 1) # axis=0 for rows, axis=1 for columns
+df = df.dropna()
+df = df.reset_index(drop=True)
+df['cum_pnl_daily'] = np.cumsum(df['pnl_daily'])
+
+# Compute Sharpe ratio and number of trades
+vec_pnl = df['pnl_daily'].values
+pnl_mean = np.round(np.mean(vec_pnl)* 252, 4) # annual PNL
+pnl_volatility = np.round(np.std(vec_pnl) * np.sqrt(252), 4) # annual PNL volatility
+sharpe = np.round(pnl_mean / pnl_volatility, 4)
+df3 = df[df['trade'] == 1]
+nb_trades = df3.shape[0]
+
+# plot cum pnl
+plot_str = 'Cumulative PNL daily ' + str(backtest.ric_long) + ' / ' + str(backtest.ric_short) + '\n'\
+    + 'PNL annual mean ' + str(pnl_mean) + '\n'\
+    + 'PNL annual volatility ' + str(pnl_volatility) + '\n'\
+    + 'PNL annual Sharpe ' + str(sharpe)
+
+plt.figure()
+plt.title(plot_str)
+plt.xlabel('Time')
+plt.ylabel('Cum PNL')
+plt.plot(df['date'], df['cum_pnl_daily'], color='blue', label='mean')
+plt.grid()
+plt.show()
